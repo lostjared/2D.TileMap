886cdb5d8320f2c18e31df02558c9d7ba45280e9
Jared Bruni <jaredbruni@gmail.com>
2023-02-25 05:03:55 -0800

    added grid

--- editor/main_window.cpp
+++ editor/main_window.cpp
@@ -1,6 +1,21 @@
 #include"main_window.hpp"
+#include<QPainter>
 
 MainWindow::MainWindow() {
     setFixedSize(1280, 720);
     setWindowTitle("Editor");
+}
+
+void MainWindow::paintEvent(QPaintEvent *p) {
+    QPainter paint(this);
+    paint.fillRect(QRect(0, 0, 1280, 720), QColor(255,255,255));
+    for(int i = 0; i < MAP_WIDTH; ++i) {
+        for(int z = 0; z < MAP_HEIGHT; ++z) {
+            int x = i*16;
+            int y = z*16;
+            x += 1;
+            y += 1;
+            paint.fillRect(QRect(x, y, 15, 15), QColor(0, 0, 0));
+        }
+    }
 }
--- editor/main_window.hpp
+++ editor/main_window.hpp
@@ -3,10 +3,14 @@
 
 #include<QMainWindow>
 
+const int MAP_WIDTH=1280/16;
+const int MAP_HEIGHT=720/16;
+
 class MainWindow : public QMainWindow {
     Q_OBJECT
 public:
     MainWindow();
+    void paintEvent(QPaintEvent *p);
     
 };
 
