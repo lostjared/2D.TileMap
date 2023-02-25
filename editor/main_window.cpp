fd3690a4ffc8fc726510be7a208cdeb8c8db6223
Jared Bruni <jaredbruni@gmail.com>
2023-02-25 04:33:56 -0800

    init

---
+++ editor/main_window.cpp
@@ -0,0 +1,6 @@
+#include"main_window.hpp"
+
+MainWindow::MainWindow() {
+    setFixedSize(1280, 720);
+    setWindowTitle("Editor");
+}
---
+++ editor/main_window.hpp
@@ -0,0 +1,15 @@
+#ifndef _EDITOR_H__
+#define _EDITOR_H__
+
+#include<QMainWindow>
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
+public:
+    MainWindow();
+    
+};
+
+
+
+#endif
