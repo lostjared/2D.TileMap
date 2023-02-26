8413c01ad360acfa719f75ab9874a598cfd61c31
Jared Bruni <jaredbruni@gmail.com>
2023-02-26 03:12:15 -0800

    tool window init

--- editor/tool_window.cpp
+++ editor/tool_window.cpp
@@ -1,1 +1,6 @@
 #include"tool_window.hpp"
+
+ToolWindow::ToolWindow(QWidget *parent) : QDialog(parent) {
+    setFixedSize(150, 480);
+    setWindowTitle("Tool Window");
+}
--- editor/tool_window.hpp
+++ editor/tool_window.hpp
@@ -1,8 +1,12 @@
 #ifndef _TOOL_WINDOW_H_
 #define _TOOL_WINDOW_H_
 
+#include<QDialog>
 
-
-
+class ToolWindow : public QDialog {
+    Q_OBJECT
+public:
+    ToolWindow(QWidget *parent);
+};
 
 #endif
