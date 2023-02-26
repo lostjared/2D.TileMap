5ba888a5aad4b90be3e9715b80cfccb7bb965736
Jared Bruni <jaredbruni@gmail.com>
2023-02-26 03:08:39 -0800

    qdialog for NewWindow

--- editor/new_window.cpp
+++ editor/new_window.cpp
@@ -1,2 +1,6 @@
 #include"new_window.hpp"
 
+NewWindow::NewWindow(QWidget *parent) : QDialog(parent) {
+    setFixedSize(640, 480);
+    setWindowTitle("New Map");
+}
--- editor/new_window.hpp
+++ editor/new_window.hpp
@@ -1,8 +1,14 @@
 #ifndef _NEW_WINDOW_H_
 #define _NEW_WINDOW_H_
 
+#include<QDialog>
 
-
+class NewWindow : public QDialog {
+    Q_OBJECT
+public:
+    NewWindow(QWidget *parent);
+    
+};
 
 
 #endif
