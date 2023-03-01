a996866ca551c30fb29e63afb3c80c0698b59e13
Jared Bruni <jaredbruni@gmail.com>
2023-03-01 03:13:32 -0800

    camera icon

--- editor/tool_window.cpp
+++ editor/tool_window.cpp
@@ -30,8 +30,16 @@ ToolWindow::ToolWindow(QWidget *parent) : QDialog(parent) {
     tool->setItemData(0, QPixmap(":/images/pencil.png"), Qt::DecorationRole);
     tool->setItemData(1, QPixmap(":/images/eraser.png"), Qt::DecorationRole);
     tool->setItemData(2, QPixmap(":/images/bucket.png"), Qt::DecorationRole);
+
+    QLabel *cam_img = new QLabel(this);
+    cam_img->setPixmap(QPixmap(":/images/camera.png"));
+    cam_img->setGeometry(5,220, 32, 32);
+    QLabel *cam_img2 = new QLabel(this);
+    cam_img2->setPixmap(QPixmap(":/images/camera.png"));
+    cam_img2->setGeometry(5, 270, 32, 32);
+
     lbl_x = new QLabel(tr("Camera X: "), this);
-    lbl_x->setGeometry(20, 230, 125, 20);
+    lbl_x->setGeometry(40, 230, 125, 20);
 
     camera_x = new QScrollBar(Qt::Orientation::Horizontal, this);
     camera_x->setGeometry(10, 250, 120, 25);
@@ -39,11 +47,14 @@ ToolWindow::ToolWindow(QWidget *parent) : QDialog(parent) {
     camera_x->setMaximum(1280);
 
     lbl_y = new QLabel(tr("Camera Y: "), this);
-    lbl_y->setGeometry(20, 280, 125, 20);
+    lbl_y->setGeometry(40, 280, 125, 20);
 
     camera_y = new QScrollBar(Qt::Orientation::Horizontal, this);
     camera_y->setGeometry(10, 300, 120, 25);
     camera_y->setMinimum(0);
     camera_y->setMaximum(720);
+
+    camera_x->setSliderPosition(0);
+    camera_y->setSliderPosition(0);
 
 }
