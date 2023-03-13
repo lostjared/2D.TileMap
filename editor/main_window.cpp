89fde7cdc9aaa6240071ede525f92299a375f86c
Jared Bruni <jaredbruni@gmail.com>
2023-03-12 19:47:54 -0700

    debug clear

--- editor/main_window.cpp
+++ editor/main_window.cpp
@@ -447,7 +447,7 @@ void MainWindow::runSettings() {
 void MainWindow::procStopped(int, QProcess::ExitStatus) {
     std::cout << proc->readAllStandardOutput().toStdString();
     run_exec->setText(tr("&Run"));
-    debug_window->Log("<span style=\"color: red;\">editor:</span> Map successfully exited. ");
+    debug_window->Log("editor: Map successfully exited. ");
     proc_run = false;
 }
 
@@ -465,7 +465,8 @@ void MainWindow::runExec() {
             proc->start(path+"/test-game", args);
             if(proc->waitForStarted()) {
                 run_exec->setText(tr("&Stop"));
-                debug_window->Log("<span style=\"color: red;\">editor:</span> Started Level: " + file_name + "\n");
+                debug_window->clear();
+                debug_window->Log("editor: Started Level: " + file_name + "\n");
                 proc_run = true;
             }
             else {
@@ -478,7 +479,7 @@ void MainWindow::runExec() {
                 msgbox.setIcon(QMessageBox::Icon::Warning);
                 msgbox.setWindowIcon(QIcon(":/images/col1.bmp"));
                 msgbox.exec();
-                debug_window->Log("<span style=\"color: red;\">editor:</span> Could not open map executable file: " + path+"/test-game");
+                debug_window->Log("editor: Could not open map executable file: " + path+"/test-game");
             }
         } else {
             proc->terminate();
