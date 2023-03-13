c3f2e08473801ba3fe834dda6a93a3996dd33863
Jared Bruni <jaredbruni@gmail.com>
2023-03-13 14:18:41 -0700

    working on gfx tool

--- gfx_file.cpp
+++ gfx_file.cpp
@@ -7,12 +7,54 @@ namespace game {
      }
      
      void GfxTable::addItem(uint32_t index, uint32_t solid, uint32_t obj, const std::string &filename) {
+        std::cout << "adding item: " << filename << " of type: " << obj << "\n";
         table.push_back( {index, solid, obj, filename });
      }
 
     bool GfxTable::buildTable(const std::string &cfg) {
         clear();
+        std::fstream file;
+        file.open(cfg, std::ios::in);
+        if(!file.is_open())
+            return false;
 
+        int type = 0;
+        while(!file.eof()) {
+            std::string line;
+            std::getline(file, line);
+            if(file) {
+                if(line == "[tiles]") {
+                    type = 0;
+                } else if(line == "[objects]") {
+                    type = 1;
+                } else if(line == "[hero:right]") {
+                    type = 2;
+                } else if(line == "[hero:left]") {
+                    type = 3;
+                } else {
+                    std::string tokens[3];
+                    auto pos = line.find(":");
+                    if(pos == std::string::npos) {
+                        std::cerr << "missing colon on type: " << type << "\n";
+                        return false;
+                    }
+                    tokens[0] = line.substr(0, pos);
+                    std::string right;
+                    right = line.substr(pos+1, line.length());
+                    pos = right.find(":");
+                    if(pos == std::string::npos) {
+                        std::cerr << "missing colon on type: " << type << "\n";
+                        return false;
+                    }
+                    tokens[1] = right.substr(0, pos);
+                    tokens[2] = right.substr(pos+1, right.length());
+                    addItem(atoi(tokens[0].c_str()), atoi(tokens[1].c_str()), type, tokens[2]);
+                }
+            }
+        }
+
+
+        file.close();
         return true;
     }
 
@@ -29,7 +71,14 @@ namespace game {
 
     bool GfxCompress::compress(const GfxTable &t) {
         for(std::vector<GfxItem>::size_type i = 0; i < t.table.size(); ++i) {
-            write_string(t.table[i].filename);
+            auto pos = t.table[i].filename.rfind("/");
+            std::string id;
+            if(pos == std::string::npos) 
+                id = t.table[i].filename;
+            else
+                id = t.table[i].filename.substr(pos+1, t.table[i].filename.length());
+            std::cout << "gfx-cmp: adding: " << t.table[i].filename << " as " << id << "\n";
+            write_string(id);
             file.write(reinterpret_cast<const char*>(&t.table[i].index), sizeof(uint32_t));
             file.write(reinterpret_cast<const char*>(&t.table[i].solid), sizeof(uint32_t));
             file.write(reinterpret_cast<const char*>(&t.table[i].obj), sizeof(uint32_t));
