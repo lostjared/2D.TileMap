02408e49b13d0884c4fb3e96e08363f7b4556850
Jared Bruni <jaredbruni@gmail.com>
2023-03-21 00:42:39 -0700

    about window

--- editor/about_window.cpp
+++ editor/about_window.cpp
@@ -1,5 +1,14 @@
 #include"about_window.hpp"
+#include<QLabel>
 
 AboutWindow::AboutWindow(QWidget *parent) : QDialog(parent) {
-    setFixedSize(320, 240);   
+    setFixedSize(320, 120);
+    setWindowTitle(tr("About"));
+    QLabel *lbl_img = new QLabel(this);
+    lbl_img->setGeometry(25, 25, 64, 64);
+    lbl_img->setPixmap(QPixmap(":/images/brick.bmp").scaled(QSize(64, 64)));
+    QLabel *lbl_txt = new QLabel(this);
+    lbl_txt->setText(tr("(C) 2023 LostSideDead Software\n\nhttp://LostSideDead.com\n"));
+    lbl_txt->setGeometry(25+64+10, 25+16, 250, 60);
+
 }
