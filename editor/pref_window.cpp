a827dfe3f7de855c85c0627c0d543ecc2380b5c7
Jared Bruni <jaredbruni@gmail.com>
2023-03-22 18:30:40 -0700

    setmainwindow

--- editor/pref_window.cpp
+++ editor/pref_window.cpp
@@ -1,6 +1,11 @@
 #include"pref_window.hpp"
+#include"main_window.hpp"
 
 PrefWindow::PrefWindow(QWidget *parent) : QDialog(parent) {
 
-    
+
+}
+
+void PrefWindow::setMainWindow(MainWindow *m) {
+    main_window = m;
 }
--- editor/pref_window.hpp
+++ editor/pref_window.hpp
@@ -4,10 +4,15 @@
 
 #include<QDialog>
 
+class MainWindow;
+
 class PrefWindow : public QDialog {
     Q_OBJECT
 public:
     PrefWindow(QWidget *parent = 0);
+    void setMainWindow(MainWindow *m);
+protected:
+    MainWindow *main_window;
 
 };
 
