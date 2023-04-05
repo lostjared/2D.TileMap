cdd9d3a018adc18f5f77fe29d84225c29bd25982
Jared Bruni <jaredbruni@gmail.com>
2023-04-05 00:33:39 -0700

    template member function

--- gfx_data.hpp
+++ gfx_data.hpp
@@ -17,6 +17,37 @@ namespace game {
         GfxData() = default;
         bool open(const std::string &filename);
         bool load(RenderObject *ro, const Color &c);
+        
+        template<typename F>
+        bool load(RenderObject *ro, const Color &c, F f) {
+            if(!file.is_open())
+                return false;
+                while(!file.eof()) {
+                    uint32_t len;
+                    file.read(reinterpret_cast<char*>(&len), sizeof(len));
+                    if(file) {
+                        char *tmp = new char [len+1];
+                        file.read(reinterpret_cast<char*>(tmp), len);
+                        tmp[len] = 0;
+                        std::string filename;
+                        filename = tmp;
+                        delete [] tmp;
+                        uint32_t index = 0, solid = 0, obj = 0, length = 0;
+                        file.read(reinterpret_cast<char*>(&index), sizeof(uint32_t));
+                        file.read(reinterpret_cast<char*>(&solid), sizeof(uint32_t));
+                        file.read(reinterpret_cast<char*>(&obj), sizeof(uint32_t));
+                        file.read(reinterpret_cast<char*>(&length), sizeof(uint32_t));
+                        char *buffer = new char [ length + 1 ];
+                        file.read(buffer, length);
+                        Image img = ro->loadImage(buffer, length, c);
+                        f(obj, img);     
+                        delete [] buffer;
+                    }
+            }
+           close();
+           return true;
+        }
+
         void close();
     protected:
         std::vector<GfxImage> gfx;
