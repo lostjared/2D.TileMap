f69f6b41b296c3e7aebd7f6ea68f2178a5f68325
Jared Bruni <jaredbruni@gmail.com>
2023-04-07 00:51:10 -0700

    indentation

--- gfx_data.hpp
+++ gfx_data.hpp
@@ -20,28 +20,30 @@ namespace game {
         
         template<typename F>
         bool load(F f) {
-            if(!file.is_open())
+            if(!file.is_open()) {
                 return false;
-                while(!file.eof()) {
-                    uint32_t len;
-                    file.read(reinterpret_cast<char*>(&len), sizeof(len));
-                    if(file) {
-                        char *tmp = new char [len+1];
-                        file.read(reinterpret_cast<char*>(tmp), len);
-                        tmp[len] = 0;
-                        std::string filename;
-                        filename = tmp;
-                        delete [] tmp;
-                        uint32_t index = 0, solid = 0, obj = 0, length = 0;
-                        file.read(reinterpret_cast<char*>(&index), sizeof(uint32_t));
-                        file.read(reinterpret_cast<char*>(&solid), sizeof(uint32_t));
-                        file.read(reinterpret_cast<char*>(&obj), sizeof(uint32_t));
-                        file.read(reinterpret_cast<char*>(&length), sizeof(uint32_t));
-                        char *buffer = new char [ length + 1 ];
-                        file.read(buffer, length);
-                        f(obj, buffer, length);     
-                        delete [] buffer;
-                    }
+            }
+
+            while(!file.eof()) {
+                uint32_t len;
+                file.read(reinterpret_cast<char*>(&len), sizeof(len));
+                if(file) {
+                    char *tmp = new char [len+1];
+                    file.read(reinterpret_cast<char*>(tmp), len);
+                    tmp[len] = 0;
+                    std::string filename;
+                    filename = tmp;
+                    delete [] tmp;
+                    uint32_t index = 0, solid = 0, obj = 0, length = 0;
+                    file.read(reinterpret_cast<char*>(&index), sizeof(uint32_t));
+                    file.read(reinterpret_cast<char*>(&solid), sizeof(uint32_t));
+                    file.read(reinterpret_cast<char*>(&obj), sizeof(uint32_t));
+                    file.read(reinterpret_cast<char*>(&length), sizeof(uint32_t));
+                    char *buffer = new char [ length + 1 ];
+                    file.read(buffer, length);
+                    f(obj, buffer, length);     
+                    delete [] buffer;
+                }
             }
            close();
            return true;
