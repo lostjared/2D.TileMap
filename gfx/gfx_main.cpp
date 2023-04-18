2086e378d15f5f7bd927398115f06bb33f6b1531
Jared Bruni <jaredbruni@gmail.com>
2023-04-18 09:03:05 -0700

    Updated command arguments for tool

--- gfx/gfx_main.cpp
+++ gfx/gfx_main.cpp
@@ -2,13 +2,39 @@
 #include<string>
 #include<sys/stat.h>
 #include "../gfx_file.hpp"
+#include<unistd.h>
 
 int main(int argc, char **argv) {
-    if(argc == 4 && std::string(argv[3]) == "-c") {
+
+        int opt = 0;
+        int mode = 0;
+        std::string input, output;
+
+        while((opt = getopt(argc, argv, "leci:o:")) != -1) {
+            switch(opt) {
+                case 'c':
+                mode = 1;
+                break;
+                case 'e':
+                mode = 2;
+                break;
+                case 'l':
+                mode = 3;
+                break;
+                case 'i':
+                input = optarg;
+                break;
+                case 'o':
+                output = optarg;
+                break;
+            }
+         }
+
+    if(mode == 1 && input.length() > 0 && output.length() > 0) {
         game::GfxTable table;
-        if(table.buildTable(argv[1])) {
+        if(table.buildTable(input)) {
             game::GfxCompress comp;
-            if(comp.open(argv[2])) {
+            if(comp.open(output)) {
                 std::cout << "gfx-cmp: Compressing table: " << argv[1] << "\n";
                 if(comp.compress(table)) {
                     comp.close();
@@ -18,28 +44,28 @@ int main(int argc, char **argv) {
                 }
             }
         }
-    } else if(argc == 4 && std::string(argv[3]) == "-e") {
+    } else if(mode == 2 && input.length() > 0 && output.length() > 0){
         game::GfxExtract ex;
         game::GfxTable table;
-        if(ex.open(argv[1])) {
-            mkdir(argv[2], S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
-            if(ex.extract(table, argv[2])) {
+        if(ex.open(input)) {
+            mkdir(output.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+            if(ex.extract(table, output)) {
                 std::cout << "gfx-cmp: Successflly Inflated: " << argv[1] << "\n";
             } else {
                 std::cout << "gfx-cmp: Failed to Inflate: " << argv[1] << "\n";
             }
         }
-    } else if(argc == 3 && std::string(argv[2]) == "-l") {
+    } else if(mode == 3 && input.length() > 0) {
         game::GfxExtract ex;
         game::GfxTable table;
-        if(ex.open(argv[1])) {
+        if(ex.open(input)) {
             if(ex.list(table)) {
                 std::cout << "successfully listed: " << table.table.size() << " items.\n";
             }
         }
     }
     else {
-        std::cout << "use:\n" << argv[0] << " table.cfg output.gfx -c\nor:\n" << argv[0] << "file.gfx direcotry -e\n";
+        std::cout << "use:\n" << argv[0] << " [-c -e -l] [-i input] [-o output]\n";
     }
     return 0;
 }
