f48014b2932db22fd4bf1b0ceb14e7741b2d100e
Jared Bruni <jaredbruni@gmail.com>
2023-04-21 05:09:18 -0700

    removed comment

--- object.cpp
+++ object.cpp
@@ -102,7 +102,6 @@ namespace game {
     void Enemy::logic(Level *level) {
         if(level->checkRect(Rect(x, y+1, 1, 3))) {
             y++;
-            std::cout << "HERE\n";
         }
     }
 
