f1eb47538d84c294f768a23a32eb851aad43076c
Jared Bruni <jaredbruni@gmail.com>
2023-04-26 02:17:08 -0700

    updated PointInRect

--- game_level.cpp
+++ game_level.cpp
@@ -312,7 +312,7 @@ namespace game {
                     resetLevel();
                 }
             } else if(objects[i]->otype == 0 && objects[i]->active) {
-                if(PointInRect(Point(hX(), hY()), Rect(objects[i]->x, objects[i]->y, 2, 2))) {
+                if(PointInRect(Point(hX(), hY()), Rect(objects[i]->x, objects[i]->y, 2, 2)) || PointInRect(Point(hX()+1, hY()), Rect(objects[i]->x, objects[i]->y, 2, 2)) || PointInRect(Point(hX()-1, hY()), Rect(objects[i]->x, objects[i]->y, 2, 2))) {
                      try {
                         Item *item = dynamic_cast<Item *>(objects[i]);
                         if(item->item_type < 6) {
