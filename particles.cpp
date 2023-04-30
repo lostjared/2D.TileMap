5f40af779cba9419198e05298b1d4cae072c91c7
Jared Bruni <jaredbruni@gmail.com>
2023-04-30 10:04:22 -0700

    comments

--- particles.cpp
+++ particles.cpp
@@ -3,10 +3,12 @@
 
 namespace game {
 
+    // add new particle
     void ParticleEmiter::addParticle(int x, int y, int type, int dir) {
         particles.push_back( { x,y,type,dir } );
     }
 
+    // set particle images
     void ParticleEmiter::setImages(const std::vector<int> &v) {
         if(!images.empty()) {
             images.erase(images.begin(), images.end());
