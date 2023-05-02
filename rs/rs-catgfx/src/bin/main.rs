a23ed8e693e9f936d5b58baab614e17a99029651
Jared Bruni <jaredbruni@gmail.com>
2023-05-02 05:54:19 -0700

    clap

--- rs/rs-catgfx/src/bin/main.rs
+++ rs/rs-catgfx/src/bin/main.rs
@@ -1,9 +1,97 @@
+use clap::{App, Arg};
+use rs_catgfx::catgfx::*;
 
-use rs_catgfx::catgfx::*; 
+
+enum Operation {
+    Concat,
+    Extract,
+    List,
+    About,
+}
+
+struct Arguments {
+    source: String,
+    dir: Option<String>, 
+    input: Option<String>,
+    mode: Operation,
+}
+
+fn parse_args() -> Arguments {
+    let m = App::new("catgfx")
+        .version("0.1.0")
+        .author("jared")
+        .arg(
+            Arg::with_name("cat")
+                .short('c')
+                .long("cat")
+                .takes_value(true)
+                .required(false)
+                .allow_invalid_utf8(true),
+        )
+        .arg(
+            Arg::with_name("extract")
+                .short('e')
+                .long("extract")
+                .takes_value(true)
+                .required(false)
+                .allow_invalid_utf8(true),
+        )
+        .arg(
+            Arg::with_name("dir")
+                .short('d')
+                .long("dir")
+                .takes_value(true)
+                .required(false)
+                .allow_invalid_utf8(true),
+        )
+        .arg(
+            Arg::with_name("input")
+                .short('i')
+                .long("input")
+                .takes_value(true)
+                .required(false)
+                .allow_invalid_utf8(true),
+        )
+        .arg(
+            Arg::with_name("list")
+                .short('l')
+                .long("list")
+                .takes_value(true)
+                .required(false)
+                .allow_invalid_utf8(true),
+        )
+        .get_matches();
+
+    let value = m.value_of_lossy("list");
+    if value != None {
+        return Arguments { source: value.unwrap().to_string(), dir: None,  input: None, mode: Operation::List }
+    }
+
+    let value = m.value_of_lossy("extract");
+    if value != None {
+        let d = m.value_of_lossy("dir").unwrap();
+        return Arguments { source: value.unwrap().to_string(),dir: Some(d.to_string()), input: None, mode: Operation::Extract }
+    }
+
+    let value = m.value_of_lossy("cat");
+    if value != None {
+        let c = m.value_of_lossy("input").unwrap();
+        return Arguments { source: value.unwrap().to_string(), dir: None, input: Some(c.to_string()), mode: Operation::Concat }
+    }
+
+    let input = String::new();
+    let m = 0;
+
+    Arguments {
+        source: input,
+        mode: Operation::About,
+        dir: None,
+        input: None,
+    }
+}
 
 fn main() -> std::io::Result<()> {
-
-
+    let args = parse_args();
 
     Ok(())
 }
--- rs/rs-catgfx/src/lib.rs
+++ rs/rs-catgfx/src/lib.rs
@@ -11,23 +11,25 @@ pub mod catgfx {
         items: Vec<GfxItem>,
     }
 
-
     impl GfxItem {
         pub fn new(i: u32, s: u32, o: u32, n: &str) -> Self {
-            GfxItem { index: i, solid: s, obj: o, name: n.to_string() }
+            GfxItem {
+                index: i,
+                solid: s,
+                obj: o,
+                name: n.to_string(),
+            }
         }
     }
 
     impl GfxTable {
         pub fn new() -> Self {
-            GfxTable {
-                items: Vec::new(),
-            }
-        }  
+            GfxTable { items: Vec::new() }
+        }
 
         pub fn add_item(&mut self, item: GfxItem) {
             self.items.push(item);
-        }  
+        }
 
         pub fn clear(&mut self) {
             self.items.clear();
@@ -35,20 +37,14 @@ pub mod catgfx {
     }
 
     pub fn cat_gfx(table: &GfxTable) -> bool {
-
-
-
         true
     }
 
     pub fn extract_gfx(table: &GfxTable) -> bool {
-
         true
     }
 
     pub fn list_gfx(table: &GfxTable) -> bool {
-
         true
     }
-
 }
