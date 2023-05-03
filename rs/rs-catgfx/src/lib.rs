716e2ec035adf47cf746f7ad86132b1081d7f926
Jared Bruni <jaredbruni@gmail.com>
2023-05-03 15:28:22 -0700

    comments

--- rs/rs-catgfx/src/lib.rs
+++ rs/rs-catgfx/src/lib.rs
@@ -4,6 +4,7 @@ pub mod catgfx {
     use byteorder::{LittleEndian, BigEndian, ReadBytesExt};
     use std::io::Read;
 
+    /// Graphics Item
     pub struct GfxItem {
         index: u32,
         solid: u32,
@@ -11,11 +12,14 @@ pub mod catgfx {
         name: String,
     }
 
+    /// Graphics Table
     pub struct GfxTable {
         items: Vec<GfxItem>,
     }
 
+    /// implementation of GfxItem
     impl GfxItem {
+        /// create new item function
         pub fn new(i: u32, s: u32, o: u32, n: &str) -> Self {
             GfxItem {
                 index: i,
@@ -26,30 +30,35 @@ pub mod catgfx {
         }
     }
 
+    /// implemtnation of GfxTable
     impl GfxTable {
+
+        /// create new table
         pub fn new() -> Self {
             GfxTable { items: Vec::new() }
         }
-
+        /// add Item to table
         pub fn add_item(&mut self, item: GfxItem) {
             self.items.push(item);
         }
-
+        /// clear table
         pub fn clear(&mut self) {
             self.items.clear();
         }
     }
-
+    /// concat graphics
     pub fn cat_gfx(input: &str, cfg_file: &str) -> bool {
         println!("catgfx: concat {} with {}", input, cfg_file);
         true
     }
 
+    /// extract graphics
     pub fn extract_gfx(input: &str, output_dir: &str) -> bool {
         println!("catgfx: extract {} to {}", input, output_dir);
         true
     }
 
+    /// list graphics in file
     pub fn list_gfx(input: &str) -> bool {
         println!("catgfx: list {}", input);
         let mut f = std::fs::File::open(input).expect("on file open");
