fe20fc8435a33d8af4fb07db4b55eb416253720c
Jared Bruni <jaredbruni@gmail.com>
2023-05-07 12:14:49 -0700

    main

---
+++ rs/tilemap/src/bin/main.rs
@@ -0,0 +1,20 @@
+
+use tilemap::tile_map::*;
+
+
+fn main() -> std::io::Result<()> {
+
+    let args : Vec<String> = std::env::args().collect();
+    let mut m = TileMap::new();
+
+    if args.len() != 2 {
+        eprintln!("Error invalid arguments....\n");
+        std::process::exit(-1);
+    }
+
+    m.load_map(&args[1])?;
+
+
+
+    Ok(())
+}
--- rs/tilemap/src/lib.rs
+++ rs/tilemap/src/lib.rs
@@ -2,24 +2,27 @@
 pub mod tile_map {
 
     #[repr(C)]
-    struct Tile {
+    pub struct Tile {
         pub color: u8,
         pub solid: u8,
         pub img: u32,
         pub layers: [u8; 3],
     }
 
-    struct TileMap {
-        tiles: Vec<Vec<Tile>>,  
+    pub struct TileMap {
+        pub tiles: Vec<Vec<Tile>>,
+        pub width: i32,
+        pub height: i32,
+        pub name: String,  
     }
 
     impl TileMap {
 
-        fn new() -> Self {
-            TileMap { tiles: Vec::new() }
+        pub fn new() -> Self {
+            TileMap { tiles: Vec::new(), width: 0, height: 0, name: String::new() }
         }
 
-        fn load_map(&mut self, file: &str) -> std::io::Result<()> {
+        pub fn load_map(&mut self, file: &str) -> std::io::Result<()> {
 
             Ok(())
         }
