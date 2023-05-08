3ef2e7d745d5f418ca4e65133f5b5b58cb700991
Jared Bruni <jaredbruni@gmail.com>
2023-05-08 14:11:35 -0700

    at function

--- rs/tilemap/src/lib.rs
+++ rs/tilemap/src/lib.rs
@@ -70,5 +70,12 @@ pub mod tile_map {
 
             Ok(())
         }
+
+        pub fn at(&self, x: i32, y: i32) -> Option<&Tile> {
+            if x >= 0 && x < self.width && y >= 0 && y < self.height {
+                Some(&self.tiles[x as usize][y as usize]);
+            }
+            None
+        }
     }
 }
