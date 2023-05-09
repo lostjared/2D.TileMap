d373acfa7da3df0dbadce00164d4a5786198021e
Jared Bruni <jaredbruni@gmail.com>
2023-05-09 16:03:13 -0700

    comments

--- rs/tilemap/src/bin/main.rs
+++ rs/tilemap/src/bin/main.rs
@@ -1,14 +1,17 @@
 use tilemap::tile_map::*;
 
 fn main() -> std::io::Result<()> {
+    // collect arguments
     let args: Vec<String> = std::env::args().collect();
-    let mut m = TileMap::new();
+    // create new tile map struct
 
+    let mut m = TileMap::new();
+    // if args not equal 2 quit
     if args.len() != 2 {
         eprintln!("Error invalid arguments....\n");
         std::process::exit(-1);
     }
-
+    // load map from file
     m.load_map(&args[1])?;
 
     Ok(())
