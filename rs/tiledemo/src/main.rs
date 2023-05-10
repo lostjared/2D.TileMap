049b0e8406805e56eafeb293d493a5696037b0d2
Jared Bruni <jaredbruni@gmail.com>
2023-05-10 15:38:25 -0700

    init level

--- rs/tiledemo/src/main.rs
+++ rs/tiledemo/src/main.rs
@@ -1,7 +1,18 @@
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use tilemap::tile_map::*;
 
-fn main() {
+fn main() -> std::io::Result<()> {
+
+    let args : Vec<String> = std::env::args().collect();
+
+    if args.len() != 2 {
+        panic!("Requires one argument");
+    }
+
+    let mut tmap : TileMap = TileMap::new();
+    tmap.load_map(&args[1])?;
+
     let width = 1280;
     let height = 720;
     let sdl = sdl2::init().unwrap();
@@ -10,7 +21,6 @@ fn main() {
     let mut can = window.into_canvas().build().map_err(|e| e.to_string()).expect("Error on canvas");
     let tc = can.texture_creator();
     let mut e = sdl.event_pump().unwrap();
-
     'main: loop {
         for _event in e.poll_iter() {
             match _event {
@@ -25,5 +35,5 @@ fn main() {
         can.clear();
         can.present();
     }
-
+    Ok(())
 }
