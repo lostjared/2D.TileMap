6f2a60d80af77b20c52e9a9057a19afee1df9a0b
Jared Bruni <jaredbruni@gmail.com>
2023-05-10 15:34:46 -0700

    initial

--- rs/tiledemo/src/main.rs
+++ rs/tiledemo/src/main.rs
@@ -1,3 +1,29 @@
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+
 fn main() {
-    println!("Hello, world!");
+    let width = 1280;
+    let height = 720;
+    let sdl = sdl2::init().unwrap();
+    let video = sdl.video().unwrap();
+    let window = video.window("Tile Demo", width, height).resizable().opengl().build().unwrap();
+    let mut can = window.into_canvas().build().map_err(|e| e.to_string()).expect("Error on canvas");
+    let tc = can.texture_creator();
+    let mut e = sdl.event_pump().unwrap();
+
+    'main: loop {
+        for _event in e.poll_iter() {
+            match _event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'main,
+                _ => {}
+            }
+        }
+        can.clear();
+        can.present();
+    }
+
 }
