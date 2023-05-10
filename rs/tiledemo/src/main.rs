96ae64743d510a20b9a03bc912f3f7e2eb1153f7
Jared Bruni <jaredbruni@gmail.com>
2023-05-10 15:48:47 -0700

    formatting

--- rs/tiledemo/src/main.rs
+++ rs/tiledemo/src/main.rs
@@ -1,45 +1,50 @@
+use rs_catgfx::catgfx::*;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use tilemap::tile_map::*;
-use rs_catgfx::catgfx::*;
 
-fn draw_map(tmap: &TileMap, surfaces: &Vec<sdl2::surface::Surface>) {
-
-}
+fn draw_map(tmap: &TileMap, surfaces: &Vec<sdl2::surface::Surface>) {}
 
 fn build_map(table: &GfxTable) -> Vec<sdl2::surface::Surface> {
-    let surf : Vec<sdl2::surface::Surface> = Vec::new();
-    for i in &table.items {
-
-    }
+    let surf: Vec<sdl2::surface::Surface> = Vec::new();
+    for i in &table.items {}
     surf
 }
 
 fn main() -> std::io::Result<()> {
-
-    let args : Vec<String> = std::env::args().collect();
+    let args: Vec<String> = std::env::args().collect();
 
     if args.len() != 3 {
         panic!("Requires one argument");
     }
 
-    let mut tmap : TileMap = TileMap::new();
+    let mut tmap: TileMap = TileMap::new();
     tmap.load_map(&args[1])?;
 
-    println!("Map loaded: [{}] - {}x{}", tmap.name, tmap.width, tmap.height);
+    println!(
+        "Map loaded: [{}] - {}x{}",
+        tmap.name, tmap.width, tmap.height
+    );
 
-    let mut table : GfxTable = GfxTable::new();
+    let mut table: GfxTable = GfxTable::new();
     build_gfx(&args[2], &mut table)?;
 
-    let surfaces : Vec<sdl2::surface::Surface> = build_map(&table);
-
-
+    let surfaces: Vec<sdl2::surface::Surface> = build_map(&table);
     let width = 1280;
     let height = 720;
     let sdl = sdl2::init().unwrap();
     let video = sdl.video().unwrap();
-    let window = video.window("Tile Demo", width, height).resizable().opengl().build().unwrap();
-    let mut can = window.into_canvas().build().map_err(|e| e.to_string()).expect("Error on canvas");
+    let window = video
+        .window("Tile Demo", width, height)
+        .resizable()
+        .opengl()
+        .build()
+        .unwrap();
+    let mut can = window
+        .into_canvas()
+        .build()
+        .map_err(|e| e.to_string())
+        .expect("Error on canvas");
     let tc = can.texture_creator();
     let mut e = sdl.event_pump().unwrap();
     'main: loop {
