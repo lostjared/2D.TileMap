0e9cb76025421756c26e60e615a908d7dd670f54
Jared Bruni <jaredbruni@gmail.com>
2023-05-24 17:08:01 -0700

    save text

--- rs/tilemap/src/lib.rs
+++ rs/tilemap/src/lib.rs
@@ -128,6 +128,12 @@ pub mod tile_map {
             Ok(())
         }
 
+        pub fn save_map_text(output: &str) -> std::io::Result<()> {
+
+
+            Ok(())
+        }
+
         /// tile at position in map
         pub fn at(&self, x: i32, y: i32) -> Option<&Tile> {
             if x >= 0 && x < self.width && y >= 0 && y < self.height {
