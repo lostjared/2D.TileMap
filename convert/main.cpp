0f6fe300c941cd1bdf4bce426a645cd2405c016b
Jared Bruni <jaredbruni@gmail.com>
2023-05-29 20:00:01 -0700

    convert

--- convert/main.cpp
+++ convert/main.cpp
@@ -1,9 +1,25 @@
 #include<iostream>
+#include"../level.hpp"
 
 int main(int argc, char **argv) {
 
+	if(argc != 3) {
+		std::cerr << "Error requires two arguments.\n";
+		exit(EXIT_FAILURE);
+	}
 
+	game::Level level;
+	if(!level.loadLevel(argv[1])) {
+		std::cerr << "Error loading level: " << argv[1] << "\n";
+		exit(EXIT_FAILURE);
+	}
 
+	if(level.saveLevelText(argv[2])) {
+		std::cout << "level saved as text: " << argv[2] << "\n";
+	} else {
+		std::cerr << "Error level " << argv[2] << " could not be saved.\n";
+	}
+	
 	return 0;
 }
 
