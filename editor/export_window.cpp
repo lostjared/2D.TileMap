13c237fb6838e0804d9cfd368ca011b5b1dfe7c6
Jared Bruni <jaredbruni@gmail.com>
2023-05-31 15:35:32 -0700

    interface

--- editor/export_window.cpp
+++ editor/export_window.cpp
@@ -1,9 +1,23 @@
 #include"export_window.hpp"
 #include<QIcon>
+#include<QComboBox>
+#include<QPushButton>
 
 // About Window constructor - (init)
 ExportWindow::ExportWindow(QWidget *parent) : QDialog(parent) {
     setFixedSize(400, 120);
     setWindowTitle(tr("Export"));
     setWindowIcon(QIcon(":/images/bluebrick.bmp"));
+    export_type = new QComboBox(this);
+    export_type->setGeometry(15, 15, 375, 25);
+    export_type->addItem(tr("Export as Text"));
+
+    export_file = new QPushButton("Export", this);
+    export_file->setGeometry(400-125, 75, 100, 25);
+
+}
+
+void ExportWindow::exportFile() {
+
+    
 }
--- editor/export_window.hpp
+++ editor/export_window.hpp
@@ -3,11 +3,20 @@
 
 #include<QDialog>
 
+class QComboBox;
+class QPushButton;
+
 class ExportWindow : public QDialog {
     Q_OBJECT
 public:
     ExportWindow(QWidget *parent = 0);
-    
+
+    QComboBox *export_type;
+    QPushButton *export_file;
+
+public slots:
+    void exportFile();
+
 };
 
 
