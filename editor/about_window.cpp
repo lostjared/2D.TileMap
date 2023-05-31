913d7f481175514721a915cbbead153fc3312f56
Jared Bruni <jaredbruni@gmail.com>
2023-05-30 18:36:05 -0700

    message

--- editor/about_window.cpp
+++ editor/about_window.cpp
@@ -10,7 +10,7 @@ AboutWindow::AboutWindow(QWidget *parent) : QDialog(parent) {
     lbl_img->setGeometry(25, 25, 64, 64);
     lbl_img->setPixmap(QPixmap(":/images/brick.bmp").scaled(QSize(64, 64)));
     QLabel *lbl_txt = new QLabel(this);
-    lbl_txt->setText(tr("(C) 2023 LostSideDead Software"));
+    lbl_txt->setText(tr("(C) 2023 LostSideDead Software - GPL v3"));
     lbl_txt->setGeometry(25+64+10, 25+16, 250, 45);
 
 }
