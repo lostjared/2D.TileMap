346efc2374bea2787702be7ba374504d8726ca9f
Jared Bruni <jaredbruni@gmail.com>
2023-06-04 15:21:35 -0700

    argument order changed

--- convert/main.cpp
+++ convert/main.cpp
@@ -3,33 +3,35 @@
 #include"../level.hpp"
 
 int main(int argc, char **argv) {
-
+	
 	if(argc != 4) {
-		std::cerr << "Error requires two arguments.\n";
+		std::cerr << "Error requires three arguments.\n";
+		std::cerr << argv[0] << " -t level.lvl level.txt\n";
+		std::cerr << argv[0] << " -b level.txt level.lvl\n";
 		exit(EXIT_FAILURE);
 	}
 
-	if(std::string(argv[3]) == "-t") {
+	if(std::string(argv[1]) == "-t") {
 		game::Level level;
-		if(!level.loadLevel(argv[1])) {
-			std::cerr << "Error loading level: " << argv[1] << "\n";
+		if(!level.loadLevel(argv[2])) {
+			std::cerr << "Error loading level: " << argv[2] << "\n";
 			exit(EXIT_FAILURE);
 		}
-		if(level.saveLevelText(argv[2])) {
-			std::cout << "level saved as text: " << argv[2] << "\n";
+		if(level.saveLevelText(argv[3])) {
+			std::cout << "level saved as text: " << argv[3] << "\n";
 		} else {
-			std::cerr << "Error level " << argv[2] << " could not be saved.\n";
+			std::cerr << "Error level " << argv[3] << " could not be saved.\n";
 		}
-	} else if (std::string(argv[3]) == "-b") {
+	} else if (std::string(argv[1]) == "-b") {
 		game::Level level;
-		if(!level.loadLevelText(argv[1])) {
+		if(!level.loadLevelText(argv[2])) {
 			std::cerr << "Error loading level text..\n";
 			exit(EXIT_FAILURE);
 		}
-		if(level.saveLevel(argv[2])) {
-			std::cout << "Level saved as bin: " << argv[2] << "\n";
+		if(level.saveLevel(argv[3])) {
+			std::cout << "Level saved as bin: " << argv[3] << "\n";
 		} else {
-			std::cerr << "Error level " << argv[2] << " could not be saved.\n";
+			std::cerr << "Error level " << argv[3] << " could not be saved.\n";
 		}
 	}
 	
